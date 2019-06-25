90f31cd3a059ab1171d2bc2abcd129284ee10164
dinstamaly <dinstamaly@gmail.com>
2019-06-25 23:50:59 +0600

    dockerize

--- tun.py
+++ tun.py
@@ -1,11 +1,15 @@
 from pytun import TunTapDevice
 
 tun = TunTapDevice(name='mytun')
+print(tun.name)
 tun.addr = '192.168.137.1'
 tun.dstaddr = '192.168.137.10'
 tun.netmask = '255.255.255.255'
 tun.mtu = 1500
-tun.persist(True)
+tap.hwaddr = '\x00\x11\x22\x33\x44\x55'
+print(tap.hwaddr)
+tun.persists(True)
 tun.up()
 buf = tun.read(tun.mtu)
 tun.write(buf)
+tun.close()
